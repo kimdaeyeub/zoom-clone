7e45a38e41a5fa07d7212061e2857af7ec0a4558
kimdaeyeub <kdy3915@naver.com>
2023-10-01 18:58:18 +0900

    프로젝트 셋팅(프론트엔드)

--- src/server.js
+++ src/server.js
@@ -1,7 +1,17 @@
 import express from "express";
 
-const app = epxress();
+const app = express();
 
-app.listen(8000, () => {
-  console.log(`✅서버는 http://localhost:8000 에서 동작중입니다.`);
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => {
+  res.render("home");
 });
+
+const handleListen = () => {
+  console.log(`✅서버는 http://localhost:8000 에서 동작중입니다.`);
+};
+
+app.listen(8000, handleListen);
