8ae2481b9a7cfc1023854d9ee767e596c6a2bd7d
kimdaeyeub <kdy3915@naver.com>
2023-10-01 23:29:24 +0900

    WebSocket messages

--- src/public/JS/app.js
+++ src/public/JS/app.js
@@ -1,1 +1,13 @@
-const webSocket = new WebSocket(`ws://${window.location.host}`);
+const socket = new WebSocket(`ws://${window.location.host}`);
+
+socket.addEventListener("open", () => {
+  console.log("Connected to Browser✅");
+});
+
+socket.addEventListener("message", (message) => {
+  console.log(message);
+});
+
+socket.addEventListener("close", () => {
+  console.log("Disconnected from server❌");
+});
--- src/server.js
+++ src/server.js
@@ -24,10 +24,12 @@ const wss = new WebSocketServer({ server });
 //websocket을 위한 서버
 // 안에 http서버를 넣음으로서 server위에 wss서버가 쌓인 형식(?)이라는데 잘 모르겠음.
 
-function handleConnection(socket) {
-  console.log(socket);
-}
-wss.on("connection", handleConnection);
+wss.on("connection", (socket) => {
+  socket.on("close", () => {
+    console.log("Disconnected from the Browser❌");
+  });
+  socket.send("Hello!");
+});
 
 server.listen(3000, handleListen);
 // http와 WebSocket을 다 이해할 수 있는 서버가 되었다.
